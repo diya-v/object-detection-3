06e4b7c59a5407fe529a9bf9891d3712b9c19066
diya-v <96090673+diya-v@users.noreply.github.com>
2023-06-13 12:41:23 -0500

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,50 @@
+status="";
+object=[];
+
+function preload(){
+    img = loadImage("kitchen.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(700,500);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Detecting Object";
+    document.getElementById("quantity").innerHTML=" 3/5 objects detected";
+}
+
+function modelLoaded(){
+    console.log("model loaded");
+    status= true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+    console.log(results);
+    object=results;
+    }
+}
+
+function draw(){
+    image(img, 0, 0, 700, 500);
+
+    if(status != "")
+    {
+        for(i=0; i<object.length; i++)
+        {
+            document.getElementById("status").innerHTML="Status: Object Detected";
+
+            fill("blue");
+            percent=floor(object[i].confidence * 100);
+            stroke("blue");
+            noFill();
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+            text(object[i].label + " "+ percent + "%", object[i].x+10, object[i].y+20);
+
+        }
+    }
+}
